9e464b0253ddf0d49686ad9d954c7af3caee23e7
kimyee <kimyee.16@gmail.com>
2014-10-09 17:51:37 -0400

    Refactoring Step #4 to use dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
-  get 'pictures/0' => 'pictures#picture0'
-  get 'pictures/1' => 'pictures#picture1'
-  get 'pictures/2' => 'pictures#picture2'
-  
+  get 'pictures/:id' => 'pictures#show', as: "picture"
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
